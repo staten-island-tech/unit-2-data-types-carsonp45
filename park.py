d6d2d9be1ee8da5f20250d6c80f03b792ac4edc2
carsonp45 <carsonp3@nycstudents.net>
2026-02-10 09:21:09 -0500

    Update park.py

--- park.py
+++ park.py
@@ -1,12 +1,16 @@
 def is_discount_allowed(is_Member, age, is_Resident):
-    is_senior_or_child = (age  < 12) or (age >= 65)
-    is_discount_allowed = input("Do you get a discount?")
-    if is_discount_allowed == "is_Member":
-        print("discount nice!")
-    elif is_discount_allowed == "is_Resident":
-        print("discount nice!")
-    elif is_discount_allowed == "is_senior_or_child":
-        print("discount nice!")
-    else: 
-        print("no discount nice!")
+    if  age  < 12 or age >= 65:
+        is_discount_allowed = input("Do you get a discount?")
+        if is_discount_allowed == "is_Member":
+            print("discount nice!")
+        elif is_discount_allowed == "is_Resident":
+            print("discount nice!")
+        elif is_discount_allowed == "is_senior_or_child":
+            print("discount nice!")
+        else: 
+            print("no discount nice!")
 is_discount_allowed(is_Member=True, age=True, is_Resident=True)
+
+
+
+
